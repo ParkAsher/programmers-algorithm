6c1ead538e7d595ec4cccf676cb13f15f08077f6
Asher Park <111232061+ParkAsher@users.noreply.github.com>
2023-04-08 16:30:33 +0900

    [level 1] Title: 예산, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,18 @@
+function solution(d, budget) {
+    var answer = 0;
+    var sum = 0;
+    
+    d.sort(function(a, b){ return a-b })
+    for(let i = 0; i < d.length; i++) {
+        sum += d[i]
+        
+        if (sum <= budget) {
+            answer+= 1
+        } else {
+            break
+        }
+        
+    }
+    
+    return answer;
+}
